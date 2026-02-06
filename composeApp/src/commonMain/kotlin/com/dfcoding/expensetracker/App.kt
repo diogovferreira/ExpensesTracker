eb02ba7e640d3738ab16bcea46535a8582d5415b
Diogo Ferreira <diogofer123@gmail.com>
2026-02-06 12:36:09 +0000

    Change repo name

--- composeApp/src/commonMain/kotlin/com/dfcoding/expensetracker/App.kt
+++ composeApp/src/commonMain/kotlin/com/dfcoding/expensetracker/App.kt
@@ -16,8 +16,8 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import modelrepocompose.composeapp.generated.resources.Res
-import modelrepocompose.composeapp.generated.resources.compose_multiplatform
+import expensetracker.composeapp.generated.resources.Res
+import expensetracker.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "ModelRepoCompose"
+rootProject.name = "ExpenseTracker"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
